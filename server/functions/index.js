a622579ad72d8a9b9543fda2916a21560fd744cc
Thevendran Marimuthu <thevenmarimuthu@gmail.com>
2018-01-27 11:24:20 +0800

    Added cloud function to summarize number of postings by positions per industry

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+const _ = require('lodash');
+const admin = require('firebase-admin');
+	
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+/*
+* Date modified		: 1/27/2018
+* Purpose			: To summarize data to visualize number of postings, grouped by positions, by every industry
+*/
+exports.numberOfPostingsByPositionsPerIndustry = functions.https.onRequest((request, response) => {
+	admin.database().ref('/data').once('value', (snapshot) => {
+		try {
+			var data = _(snapshot.val())
+						.groupBy('industry')
+						.map(function(value, key) {
+							var obj = {};
+							obj[key] = _.countBy(value, 'position')
+							return obj
+						}).value();
+        	response.status(200).send(data);
+		} catch (e) {
+			
+		}
+ 	});
+});
