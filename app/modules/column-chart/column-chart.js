277b4b8bf4fc79886088d1df33303566f6881f22
Thevendran Marimuthu <thevenmarimuthu@gmail.com>
2018-01-27 15:14:49 +0800

    Enabled first chart

--- app/modules/column-chart/column-chart.js
+++ app/modules/column-chart/column-chart.js
@@ -22,38 +22,39 @@ define([
                 })
                 .then(function(response) {
                     var data = response.data;
-                    console.log(data)
 
                     var categories = [];
+                    var object = {};
 
                     _.forEach(data, function(item) {
-                        categories.push(Object.keys(item));
+                        var key = Object.keys(item)[0];
+                        categories.push(key);
+
+                        object = _.mergeWith(object, item[key], function customizer(objValue, srcValue) {
+                            if (_.isArray(objValue)) {
+                                return objValue.concat(srcValue);
+                            } else {
+                                return [].concat(srcValue);
+                            }
+                        });
                     });
 
-                    console.log(categories);
+                    var series = [];
+
+                    _.forEach(object, function(value, key){
+                        series.push({ name: key, data: value });
+                    });
                     
                     Highcharts.chart(element[0], {
                         chart: {
-                            type: 'column'
+                            type: 'column',
+                            height: '100%'
                         },
                         title: {
                             text: 'Number of postings (by positions) over industries'
                         },
                         xAxis: {
-                            categories: [
-                                'Jan',
-                                'Feb',
-                                'Mar',
-                                'Apr',
-                                'May',
-                                'Jun',
-                                'Jul',
-                                'Aug',
-                                'Sep',
-                                'Oct',
-                                'Nov',
-                                'Dec'
-                            ],
+                            categories: categories,
                             title: 'Industries',
                             crosshair: true
                         },
@@ -77,22 +78,8 @@ define([
                                 borderWidth: 0
                             }
                         },
-                        series: [{
-                            name: 'Tokyo',
-                            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
 
-                        }, {
-                            name: 'New York',
-                            data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
-                        }, {
-                            name: 'London',
-                            data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-                        }, {
-                            name: 'Berlin',
-                            data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-                        }]
+                        series: series
                     });
                 }, function(error) {
                     console.error(error);
