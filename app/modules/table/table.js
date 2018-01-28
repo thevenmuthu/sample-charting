f26788aee05caf2fc1dc747c3c310c939cbe56ac
Thevendran Marimuthu <thevenmarimuthu@gmail.com>
2018-01-28 15:25:22 +0800

    Added add record fields

--- app/modules/table/table.js
+++ app/modules/table/table.js
@@ -11,32 +11,49 @@ define([
     function TableDirective($http) {
         return {
             restrict: 'E',
-            scope: {
-                model: '='
-            },
+            scope: {},
             templateUrl: 'modules/table/table.html',
             link: function(scope, element, attrs) {
                 scope.gridOptions = {};
                 scope.showTable = false;
+                scope.format = 'dd-MM-yyyy';
+                scope.openTimepicker = function() {
+                    scope.opened = true;
+                };
 
-                $http({
-                    method: 'GET',
-                    url: 'https://us-central1-sample-charting-2.cloudfunctions.net/postingData'
-                })
-                .then(function(response) {
-                    scope.showTable = true;
-                    scope.gridOptions = {
-                        data: response.data,
-                        enableSorting: true,
-                        enableCellEditOnFocus: true
-                    };
+                scope.resetFields = function() {
+                  
+                };
 
-                }, function(error) {
-                    console.error(error);
-                });
+                scope.addRecord = function() {
+
+                };
+
+                // $http({
+                //     method: 'GET',
+                //     url: 'https://us-central1-sample-charting-2.cloudfunctions.net/postingData'
+                // })
+                // .then(function(response) {
+                //     scope.showTable = true;
+                //     scope.gridOptions = {
+                //         data: response.data,
+                //         enableSorting: true,
+                //         enableCellEditOnFocus: true
+                //     };
+
+                // }, function(error) {
+                //     console.error(error);
+                // });
             }
         };
     }
+
+    //  function disabled(data) {
+    //     console.log(data)
+    //     var date = data.date,
+    //     mode = data.mode;
+    //     return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+    // }
 
     tableModule.directive('tableDirective', TableDirective);
 });
